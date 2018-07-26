987587342d58748fe05891658cadada997c52379
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 18:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(line, name) {
 }
 
 function nowServing() {
-  if (line.length === 0) {
+  if (!line.length) {
     return `There is nobody waiting to be served!`
   } 
   return `Currently serving ${line.shift()}.`
