65455fd8a2a07e4ab985be616cc9e9af6b897c32
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 18:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ function takeANumber(katzDeliLine, name) {
 
 
 
+
+// describe('deli', () => {
+//   describe('takeANumber', () => {
+//     var katzDeli;
+//     var otherDeli;
+
+//     beforeEach(() => {
+//       katzDeli = [];
+//       otherDeli = ["Steven", "Blake", "Avi"];
+//     });
+
+//     it('adds a person to the line', () => {
+//       expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, Ada. You are number 1 in line.");
+//       expect(katzDeli).toEqual(['Ada']);
+//     });
+
+//     it('appends the person the end of the line if there are already people on it', () => {
+//       expect(takeANumber(otherDeli, 'Grace')).toEqual("Welcome, Grace. You are number 4 in line.");
+//       expect(otherDeli).toEqual(["Steven", "Blake", "Avi", "Grace"]);
+//     });
+
+//     it("properly handles multiple people being added", () => {
+//       takeANumber(katzDeli, 'Ada');
+//       takeANumber(katzDeli, 'Grace');
+//       takeANumber(katzDeli, 'Kent');
+
+//       expect(katzDeli).toEqual(["Ada", "Grace", "Kent"]);
+//     });
+//   });
+
+
 // function currentLine(line) {
 //   if (!line.length) {
 //     return "The line is currently empty."
