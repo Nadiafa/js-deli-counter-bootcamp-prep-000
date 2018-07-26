6db7e4ccdd750a5a09159ef4fe64eb0c48c41477
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 18:39:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function currentLine(line) {
   if (line.length === 0) {
     return `The line is currently empty.`
   } 
-  
+  var katzDeliLine = []
+  for (var i=0, l=line.length; i<l; i++){
+    katzDeliLine.push(`${i++}. ${line[i]}`)
+  } 
+  return `the 
       
 }
 
