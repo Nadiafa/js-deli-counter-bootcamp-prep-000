324757dce9a34da5818f51e9cdc15b6ca403f947
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 18:06:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
  for (var i=0; i<katzDeliLine.length; i++) {
-   katzDeli[i].push(name)
+   katzDeliLine[i].push(katzDeli.name)
  } 
  return katzDeli
 }
