1f2e6a2052306a8fe9501f111f56b2e193a84e7b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 18:41:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,19 +21,3 @@ function currentLine(line) {
   return `The line is currently: ${katzDeliLine.join(', ')}` 
       
 }
-
-
-
-// function currentLine(line) {
-//   if (!line.length) {
-//     return "The line is currently empty."
-//   }
-
-//   const numbersAndNames = []
-
-//   for (let i = 0, l = line.length; i < l; i++) {
-//     numbersAndNames.push(`${i + 1}. ${line[i]}`)
-//   }
-
-//   return `The line is currently: ${numbersAndNames.join(', ')}`
-// };
