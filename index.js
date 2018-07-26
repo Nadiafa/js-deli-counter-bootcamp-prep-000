77e44623363a8836593effa9834b4e9153de4e78
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 18:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,14 +22,30 @@ function currentLine(line) {
 
 
 
-// describe('nowServing', () => {
-//     it('returns the line is empty when no one is on line', () => {
-//       expect(nowServing([])).toEqual("There is nobody waiting to be served!");
-//     });
+// function currentLine(line) {
+//   if (!line.length) {
+//     return "The line is currently empty."
+//   }
 
-//     it('returns an announcement about the person it is serving, and shifts the line', () => {
-//       const deliLine = ["Steven", "Blake", "Avi"]
-//       expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
-//       expect(deliLine).toEqual(["Blake", "Avi"]);
-//     });
-//   });
+//   const numbersAndNames = []
+
+//   for (let i = 0, l = line.length; i < l; i++) {
+//     numbersAndNames.push(`${i + 1}. ${line[i]}`)
+//   }
+
+//   return `The line is currently: ${numbersAndNames.join(', ')}`
+// };
+
+// function nowServing(line) {
+//   if (!line.length) {
+//     return "There is nobody waiting to be served!"
+//   }
+
+//   return `Currently serving ${line.shift()}.`
+// };
+
+// function takeANumber(line, name) {
+//   line.push(name)
+
+//   return `Welcome, ${name}. You are number ${line.length} in line.`
+// };
