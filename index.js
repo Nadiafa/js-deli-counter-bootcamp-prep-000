c9e05e11814139e8749fb1e510b1b4ced6412862
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 18:27:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ function takeANumber(line, name) {
 }
 
 function nowServing() {
-  if (!line) {
+  if (line.length === 0) {
     return `There is nobody waiting to be served!`
   } else {
-      return line.shift()
+      return deliLine.shift()
     }
     
   }
+
+
+
+
+describe('nowServing', () => {
+    it('returns the line is empty when no one is on line', () => {
+      expect(nowServing([])).toEqual("There is nobody waiting to be served!");
+    });
+
+    it('returns an announcement about the person it is serving, and shifts the line', () => {
+      const deliLine = ["Steven", "Blake", "Avi"]
+      expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
+      expect(deliLine).toEqual(["Blake", "Avi"]);
+    });
+  });
